61f58d37c75668f558dae20db22675ed80fac427
Loxico <paolocuzcanogamboa@gmail.com>
2026-05-16 01:02:01 -0500

    remove dotenv logs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import path from "node:path";
-dotenv.config({ path: path.resolve(process.cwd(), ".env") });
+dotenv.config({ path: path.resolve(process.cwd(), ".env"), quiet: true });
 
 import fs from "node:fs";
 import Database from "./db/database";
